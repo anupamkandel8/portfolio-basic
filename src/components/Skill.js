278a5c5b8ef1e2445e1baa431f5c81185a8b47fd
Anupam <anupamkandel8@gmail.com>
2025-06-18 19:58:56 +0545

    a

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -12,9 +12,18 @@ export default function Skill() {
             <span className="font-semibold mr-2">{category.category + ":"}</span>
             <span className="flex flex-wrap gap-3 mt-0">
               {category.items.map((skill, i) => (
-                <span key={i} className="flex items-center gap-1">
+                <span
+                  key={i}
+                  className="relative flex items-center gap-1 group "
+                  tabIndex={0}
+                >
                   <img src={skill.logo} alt={skill.name} className="w-6 h-6" />
-                  {/* <span>{skill.name}</span> */}
+                  {/* Tooltip */}
+                  <span
+                    className="absolute left-1/2 -translate-x-1/2 bottom-full mb-2 px-2 py-1 rounded bg-gray-800 text-white text-xs opacity-0 group-hover:opacity-100 group-focus:opacity-100 pointer-events-none transition-opacity z-10 whitespace-nowrap"
+                  >
+                    {skill.name}
+                  </span>
                 </span>
               ))}
             </span>
