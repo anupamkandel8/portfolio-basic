639e7c89f93068e4e21ac1c1531ae4a8c159fd95
Anupam <anupamkandel8@gmail.com>
2025-06-19 13:59:40 +0545

    a

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function page() {
     <>
       <Toaster />
 
-      <div className=" mx-auto px-4 sm:px-6 lg:px-8">
+      <div className=" flex flex-col mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid  grid-cols-1 sm:grid-cols-2 gap-3 justify-center items-center lg:px-10">
           <Profile className="  lg:min-h-screen" />
           <div>
@@ -27,12 +27,12 @@ export default function page() {
         <Project />
 
         <Skill className=" sm:hidden justify-center" />
-        <div
-          className=" max-w-fit mx-auto m-8 bg-gray-100 text-black px-4 py-2 rounded-full shadow-lg cursor-pointer hover:bg-gray-400 transition-all duration-300  hover:scale-105"
+        <button
+          className=" max-w-fit mx-auto m-8 bg-gray-100 text-black px-4 py-2 rounded-full shadow-lg cursor-pointer hover:bg-gray-400 transition-all duration-300  hover:scale-105 "
           onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         >
           Contact Me!
-        </div>
+        </button>
       </div>
     </>
   );
